cb291db266d050c0dd445d4c805499c1f8b81b63
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-05-26 16:39:17 +0300

    modified some things for fly.io deployement

--- src/devops/contacts/views.py
+++ src/devops/contacts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_protect
 from .models import Contact
 
 # Create your views here.
 
+@csrf_protect
 def indexView(request):
     """Render the front page of the application"""
 
@@ -14,6 +16,7 @@ def indexView(request):
         {"contacts": contacts}
     )
 
+@csrf_protect
 def addContactView(request):
     """Create a new blog"""
 
@@ -25,9 +28,12 @@ def addContactView(request):
             name=name,
             number=number
         )
+    
+    contacts = Contact.objects.all()
 
-    return redirect("/")
+    return render(request, "index.html", {"contacts": contacts})
 
+@csrf_protect
 def removeContactsView(request):
     """Create a new blog"""
 
--- src/devops/devops/settings.py
+++ src/devops/devops/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,3 +128,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+    "https://devops-valtteri1.fly.dev"
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://0.0.0.0:8000",
+    "https://devops-valtteri1.fly.dev"
+]
