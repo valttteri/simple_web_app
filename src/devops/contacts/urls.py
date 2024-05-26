9ab9ccd010e87f09225f715a42eadc33e4b3bc7b
Valtteri Ahola <valtteri.ahola@helsinki.fi>
2024-05-26 14:42:27 +0300

    added app logic and poetry

--- src/devops/contacts/urls.py
+++ src/devops/contacts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.indexView, name='frontpage') # the frontpage
+    path('', views.indexView, name='frontpage'), # the frontpage
+    path('addcontact', views.addContactView, name='addcontact'), # add a new contact
+    path('removecontacts', views.removeContactsView, name='removecontacts'), # delete all contacts
 ]
--- src/devops/contacts/views.py
+++ src/devops/contacts/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 # Create your views here.
 
 def indexView(request):
     """Render the front page of the application"""
 
-    contacts = ["mikko", "pekka"]
+    contacts = Contact.objects.all()
 
     return render(
         request,
         "index.html",
         {"contacts": contacts}
     )
+
+def addContactView(request):
+    """Create a new blog"""
+
+    if request.method == "POST":
+        name = request.POST.get("name", "").strip()
+        number = request.POST.get("number", "").strip()
+    
+        Contact.objects.create(
+            name=name,
+            number=number
+        )
+
+    return redirect("/")
+
+def removeContactsView(request):
+    """Create a new blog"""
+
+    if request.method == "POST":
+
+        Contact.objects.all().delete()
+
+    return redirect("/")
